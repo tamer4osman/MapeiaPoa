7cd6173906391157fb146d7e5d1e7dee355c0efa
tutabeier <lucas.beier@gmail.com>
2016-02-18 23:48:09 -0200

    Gitignore, gradle build and basic dir structure.

---
+++ src/main/java/org/mapeiapoa/MapeiapoaApplication.java
@@ -0,0 +1,12 @@
+package org.mapeiapoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeiapoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeiapoaApplication.class, args);
+	}
+}
