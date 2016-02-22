b3ec2bea909242f259da4f3c2174f5780e43dfc4
tutabeier <lucas.beier@gmail.com>
2016-02-21 22:11:10 -0300

    Adds support for war

--- src/main/java/org/mapeiapoa/MapeiapoaApplication.java
+++ src/main/java/org/mapeiapoa/MapeiapoaApplication.java
@@ -2,9 +2,16 @@ package org.mapeiapoa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MapeiapoaApplication {
+public class MapeiapoaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MapeiapoaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MapeiapoaApplication.class, args);
